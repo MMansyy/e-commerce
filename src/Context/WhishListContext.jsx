fa357575ee8c4f7aec7cc342cad5eded1b9020fd
MMansyy <hessenmansyy@gmail.com>
2025-02-24 02:32:55 +0200

    final 4

--- src/Context/WhishListContext.jsx
+++ src/Context/WhishListContext.jsx
@@ -1,41 +1,47 @@
 import axios from "axios";
-import { useEffect } from "react";
+import { useContext, useEffect } from "react";
 import { createContext } from "react";
+import { TokenContext } from "./TokenContext";
 
 
 
 export const WhishListContext = createContext(undefined);
 
-const headers = {
-    token: localStorage.getItem('token')
-}
-
-console.log(headers);
-
-
-function addToWhishList(id) {
-    return axios.post('https://ecommerce.routemisr.com/api/v1/wishlist'
-        , { productId: id }, { headers })
-        .then(res => res.data)
-        .catch(err => err.response.data)
-
-}
-
-function getWhishList() {
-    return axios.get('https://ecommerce.routemisr.com/api/v1/wishlist', { headers })
-        .then(res => res.data)
-        .catch(err => err.response.data)
-}
-
-function removeFromWhishList(id) {
-    return axios.delete(`https://ecommerce.routemisr.com/api/v1/wishlist/${id}`, { headers })
-        .then(res => res.data)
-        .catch(err => err.response.data)
-}
 
 
 
 export function WhishListProvider({ children }) {
+
+    const { token: hamada } = useContext(TokenContext)
+
+    const headers = {
+        token: hamada
+    }
+
+    console.log(headers);
+
+
+
+    function addToWhishList(id) {
+        return axios.post('https://ecommerce.routemisr.com/api/v1/wishlist'
+            , { productId: id }, { headers })
+            .then(res => res.data)
+            .catch(err => err.response.data)
+
+    }
+
+    function getWhishList() {
+        return axios.get('https://ecommerce.routemisr.com/api/v1/wishlist', { headers })
+            .then(res => res.data)
+            .catch(err => err.response.data)
+    }
+
+    function removeFromWhishList(id) {
+        return axios.delete(`https://ecommerce.routemisr.com/api/v1/wishlist/${id}`, { headers })
+            .then(res => res.data)
+            .catch(err => err.response.data)
+    }
+
 
 
     return (
